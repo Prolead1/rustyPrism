83d3cee15e44f3c255d166b6947efb76a1d374f5
Rahul Thaker <rahul1thaker@gmail.com>
2023-05-01 23:19:14 +0800

    Update connector to use dynamic buffer

--- src/connector.rs
+++ src/connector.rs
@@ -49,42 +49,55 @@ impl FixMsgConnector {
         processor: Arc<Mutex<FixMsgProcessor>>,
         stream: Arc<Mutex<TcpStream>>,
     ) {
-        let mut buffer = [0u8; 1024];
+        let mut buffer = Vec::new();
+
         let mut stream = stream.lock().await;
 
-        if let Ok(bytes_read) = stream.read(&mut buffer).await {
-            if bytes_read == 0 {
-                return;
-            }
-            let mut remaining = String::from_utf8_lossy(&buffer[..bytes_read]).to_string();
+        loop {
+            let mut chunk = vec![0u8; 1024];
 
-            let remaining_clone = remaining.clone();
-            while let Some(index) = remaining.find('\x01') {
-                let (current_message, _) = remaining_clone.split_at(index);
-                println!(
-                    "[CONNECTOR] Processing message: {} from client: {}",
-                    current_message,
-                    match stream.peer_addr() {
-                        Ok(addr) => addr,
-                        Err(err) => {
-                            eprintln!("[CONNECTOR] Error getting peer address: {}", err);
-                            std::net::SocketAddr::from(([0, 0, 0, 0], 0))
-                        }
+            match stream.read(&mut chunk).await {
+                Ok(bytes_read) => {
+                    if bytes_read == 0 {
+                        break;
                     }
-                );
-                remaining = remaining[index + 1..].to_string();
 
-                // Spawn a non-blocking async task to process the message.
-                let processor_clone = Arc::clone(&processor);
-                let message_clone = current_message.to_string();
-                tokio::spawn(async move {
-                    println!(
-                        "[CONNECTOR] Creating processor thread for message: {}",
-                        message_clone
-                    );
-                    let mut processor = processor_clone.lock().await;
-                    processor.process_message(message_clone).await;
-                });
+                    buffer.extend_from_slice(&chunk[..bytes_read]);
+
+                    while let Some(index) = buffer.iter().position(|&byte| byte == b'\x01') {
+                        let current_message = buffer[..index].to_owned();
+                        buffer = buffer[index + 1..].to_owned();
+
+                        let message_str = String::from_utf8_lossy(&current_message).to_string();
+
+                        println!(
+                            "[CONNECTOR] Processing message: {} from client: {}",
+                            message_str,
+                            match stream.peer_addr() {
+                                Ok(addr) => addr,
+                                Err(err) => {
+                                    eprintln!("[CONNECTOR] Error getting peer address: {}", err);
+                                    std::net::SocketAddr::from(([0, 0, 0, 0], 0))
+                                }
+                            }
+                        );
+
+                        let processor_clone = Arc::clone(&processor);
+                        let message_clone = message_str.clone();
+                        tokio::spawn(async move {
+                            println!(
+                                "[CONNECTOR] Creating processor thread for message: {}",
+                                message_clone
+                            );
+                            let mut processor = processor_clone.lock().await;
+                            processor.process_message(message_clone).await;
+                        });
+                    }
+                }
+                Err(err) => {
+                    eprintln!("[CONNECTOR] Error reading from stream: {}", err);
+                    break;
+                }
             }
         }
     }
